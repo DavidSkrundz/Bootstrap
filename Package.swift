fe67a1dd70ec17e05751a512be9282dddf627762
David Skrundz <david@skrundz.ca>
2017-02-01 12:55:35 -0700

    Change dependency information

--- Package.swift
+++ Package.swift
@@ -12,9 +12,9 @@ let package = Package(
 		Target(name: "Build", dependencies: []),
 	],
 	dependencies: [
-		.Package(url: "https://github.com/DavidSkrundz/CommandLine.git", versions: Version(1,0,0)..<Version(1,1,0)),
-		.Package(url: "https://github.com/DavidSkrundz/LibC.git", versions: Version(1,0,0)..<Version(1,1,0)),
-		.Package(url: "https://github.com/DavidSkrundz/UnicodeOperators.git", versions: Version(1,0,0)..<Version(1,1,0)),
-		.Package(url: "https://github.com/DavidSkrundz/Util.git", versions: Version(1,0,0)..<Version(1,1,0)),
+		.Package(url: "https://github.com/DavidSkrundz/CommandLine.git", majorVersion: 1, minor: 0),
+		.Package(url: "https://github.com/DavidSkrundz/LibC.git", majorVersion: 1, minor: 0),
+		.Package(url: "https://github.com/DavidSkrundz/UnicodeOperators.git", majorVersion: 1, minor: 0),
+		.Package(url: "https://github.com/DavidSkrundz/Util.git", majorVersion: 1, minor: 0),
 	]
 )
