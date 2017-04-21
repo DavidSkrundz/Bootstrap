291b169474d78b7c0642185f579e13bf9c98f370
David Skrundz <david@skrundz.ca>
2017-04-21 15:21:12 -0600

    Update to Swift 3.1 and Xcode 8.3

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
 		Target(name: "Build", dependencies: []),
 	],
 	dependencies: [
-		.Package(url: "https://github.com/DavidSkrundz/CommandLine.git", majorVersion: 1, minor: 0),
+		.Package(url: "https://github.com/DavidSkrundz/CommandLine.git", majorVersion: 1, minor: 1),
 		.Package(url: "https://github.com/DavidSkrundz/LibC.git", majorVersion: 1, minor: 0),
 		.Package(url: "https://github.com/DavidSkrundz/UnicodeOperators.git", majorVersion: 1, minor: 0),
 		.Package(url: "https://github.com/DavidSkrundz/Util.git", majorVersion: 1, minor: 0),
