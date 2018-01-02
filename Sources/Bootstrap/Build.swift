83717714cbc68b21bc698de914279e58cbbd9d73
David Skrundz <david@skrundz.ca>
2018-01-02 00:11:33 -0700

    Update for Ubuntu 16.04

--- Sources/Bootstrap/Build.swift
+++ Sources/Bootstrap/Build.swift
@@ -6,6 +6,6 @@
 import CommandLine
 
 internal func build(_ bootstrapDirectory: String, _ flags: [String]) {
-	let task = AutoTask(cmd: bootstrapDirectory + "/build", args: flags)
+	let task = AutoTask(cmd: bootstrapDirectory + "/bin/build", args: flags)
 	task > standardOut
 }
